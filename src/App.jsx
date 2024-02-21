57a6c5c9987c664354ff3e3826477076467dfa24
rijalsanjit@gmail.com <rijalsanjit@gmail.com>
2024-02-21 20:48:25 +1100

    first commit project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>welcome to my page</h1>
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
